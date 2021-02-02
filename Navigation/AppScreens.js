53c66a377c9fcf90cd946a781ad0f8cdfcafe864
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-02-02 19:25:05 +0530

    Fetched Data Successful

---
+++ Navigation/AppScreens.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from '../screens/Homescreen';
+import Result from '../screens/Result';
+
+const Stack = createStackNavigator();
+
+function AppScreen() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Result" component={Result} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppScreen;
